865be2c635de4507b9e03fb7b891ec63e9f356e8
Suchen Oguri <suchen.oguri@gmail.com>
2017-10-31 23:41:58 +0530

    Add files via upload

--- Collections/HelloLinkedHashSet.java
+++ Collections/HelloLinkedHashSet.java
@@ -10,6 +10,17 @@ public class HelloLinkedHashSet {
 		System.out.println("LinkedHashSet class extends HashSet class which implements Set interface");
 		
 		
+		Employee empObj = new Employee("Suchen","675259",100);
+		Employee empObj2 = new Employee("Oguri","675260",100);
+		
+		
+		sampleSet.add(empObj);
+		sampleSet.add(empObj2);
+		
+		for(Object o:sampleSet) {
+			System.out.println("\t"+o);
+		}
+		
 		
 		
 		
